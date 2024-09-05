e2696e92e57739ecce0204926d4c3c29aa7b7d7e
SaiThaw <u6520152@au.edu>
2024-09-05 17:04:54 +0700

    initial commit

--- app/about/page.js
+++ app/about/page.js
@@ -1,8 +1,10 @@
 export default function Home({ params, searchParams }) {
     console.debug({params})
     console.debug({searchParams})
-    const name = searchParams['name'] || 'World';   
+    const name = searchParams['name'];   
     return (
       <h1>Hello {name}</h1>
     );
   }
+
+  
